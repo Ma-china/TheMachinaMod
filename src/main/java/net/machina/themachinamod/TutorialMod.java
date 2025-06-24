651cfc738e6e10c3b01d56ef9e42fb1e305ff7b4
Ricky Hao <rickyhao2021.nl@outlook.com>
2025-06-24 23:06:29 +0200

    Added hammer and sickle item

--- src/main/java/net/machina/themachinamod/TutorialMod.java
+++ src/main/java/net/machina/themachinamod/TutorialMod.java
@@ -1,6 +1,8 @@
 package net.machina.themachinamod;
 
 import com.mojang.logging.LogUtils;
+import net.machina.themachinamod.items.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -30,6 +32,8 @@ public class TutorialMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
 
 
         // Register the item to a creative tab
@@ -45,6 +49,9 @@ public class TutorialMod {
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
 
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.HAMMERANDSICKLE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
